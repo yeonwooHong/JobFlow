9f72c117cafdecb0ab0782dbeaeb324cccdb3fcb
yeonu <yeonu@Yeonu-2.local>
2025-11-29 16:58:40 -0500

    Add: Added a feature saving fetched job data to DB

--- backend/services/jobService.js
+++ backend/services/jobService.js
@@ -1,27 +1,69 @@
 import axios from "axios";
 import dotenv from "dotenv";
+import supabase from "../supabaseClient.js";
 
 dotenv.config();
 
+// Request Job data from the OpenAPI
 export async function fetchJobs(query) {
-  try {
-    const response = await axios.get(process.env.RAPIDAPI_URL, {
-      params: {
-        query, // Refactor: User's initial setting
-        page: "1", // 1 page 10 items
-        num_pages: "1", // 1 - 50
-        country: "us",
-        date_posted: "all", // all, today, 3days, week, month
-      },
-      headers: {
-        "x-rapidapi-key": process.env.RAPIDAPI_KEY,
-        "x-rapidapi-host": process.env.RAPIDAPI_HOST,
-      },
-    });
+    try {
+        const response = await axios.get(process.env.RAPIDAPI_URL, {
+            params: {
+                query, // Refactor: User's initial setting
+                page: "1", // 1 page 10 items
+                num_pages: "1", // 1 - 50
+                country: "ca",
+                date_posted: "all", // all, today, 3days, week, month
+            },
+            headers: {
+                "x-rapidapi-key": process.env.RAPIDAPI_KEY,
+                "x-rapidapi-host": process.env.RAPIDAPI_HOST,
+            },
+        });
 
-    return response.data;
-  } catch (err) {
-    console.error("API error:", err.message);
-    throw err;
-  }
+        // return response.data;
+        await saveJobsToSupabase(response.data);
+    } catch (err) {
+        console.error("Fail to fetch job data:", err.message);
+        throw err;
+    }
+}
+
+// Save data to DB
+export async function saveJobsToSupabase(jobs) {
+    try {
+        // Map data fields
+        const jobsArray = jobs.data.map(job => ({
+            title: job.job_title,
+            description: job.job_description,
+            link: job.job_apply_link,
+            employment_type: job.job_employment_type,
+            employer_name: job.employer_name,
+            country_code:job.job_country,
+            city: job.job_city,
+            state: job.job_state,
+            location: job.job_location,
+            latitude: job.job_latitude,
+            longitude: job.job_longitude,
+            is_remote: job.job_is_remote,
+            posted_at: job.job_posted_at_datetime_utc,
+        }))
+
+        // Insert data into DB
+        // Refactor: Should check the duplicated rows: job_id
+        const { data, error } = await supabase
+            .from('jobs')
+            .insert(jobsArray)
+            .select();
+
+        if (error) {
+            console.error("Error occurs while inserting data into Supabase:", error);
+            throw error;
+        }
+        
+        console.log(`Successfully inserted ${data.length} jobs into Supabase.`);
+    } catch (err) {
+        console.error("Fail to save job data:", err.message);
+        throw err;
+    }
 }
--- backend/supabaseClient.js
+++ backend/supabaseClient.js
@@ -4,8 +4,8 @@ import dotenv from 'dotenv';
 dotenv.config(); // .env file load
 
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URLL,
-  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY // To bypass Row Level Security
 );
 
 export default supabase;
